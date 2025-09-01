6237343a521e0fefa5a87db6bc2755d1a409048d
Braunerbaerr <danielkwiatkowski@gmx.de>
2025-09-01 11:42:06 +0200

    background hinmzugefügt

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,13 +1,12 @@
-class Character extends MovableObject{
-
+class Character extends MovableObject {
   constructor() {
     super();
-    this.loadImage('img/2_character_pepe/2_walk/W-21.png');
+    this.loadImage("img/2_character_pepe/2_walk/W-21.png");
+    this.x = 120;
+    this.y = 200;
+    this.width = 100;
+    this.height = 150;
   }
-  
 
-
-  jump(){
-    
-  }
+  jump() {}
 }
--- classes/world.class.js
+++ classes/world.class.js
@@ -1,16 +1,36 @@
 class World {
-  charakter = new Character();
-  enemies = [new Chicken(), new Chicken(), new Chicken()];
-
+  charakter   = new Character();
+  enemies     = [new Chicken(), new Chicken(), new Chicken()];
+  clouds      = [new Cloud(1), new Cloud(2)];
+  backgrounds = Background.allLayers();
+  
   constructor(canvas) {
     this.ctx = canvas.getContext("2d");
-    this.canvas = canvas; 
+    this.canvas = canvas;
     this.draw();
   }
 
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    
+    this.backgrounds.forEach(bg => {
+      this.ctx.drawImage(bg.img, bg.x, bg.y, bg.width, bg.height);
+    });
+    
+
+    
+    this.clouds.forEach((cloud) => {
+      this.ctx.drawImage(
+        cloud.img,
+        cloud.x,
+        cloud.y,
+        cloud.width,
+        cloud.height
+      );
+    });
+
+    
     this.ctx.drawImage(
       this.charakter.img,
       this.charakter.x,
@@ -19,7 +39,8 @@ class World {
       this.charakter.height
     );
 
-    this.enemies.forEach(enemy => {
+  
+    this.enemies.forEach((enemy) => {
       this.ctx.drawImage(
         enemy.img,
         enemy.x,
@@ -30,7 +51,7 @@ class World {
     });
 
     let self = this;
-    requestAnimationFrame(function() {
+    requestAnimationFrame(function () {
       self.draw();
     });
   }
